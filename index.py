8202e95d0316a4d6c45107572a94143db549bd18
Mehek Bhatnagar <mehekbhatnagar3@gmail.com>
2026-02-19 18:20:06 -0800

    updates to frequency and importance count when needed

--- index.py
+++ index.py
@@ -18,7 +18,7 @@ class Index:
         self.index = shelve.open('index.db')
         self.index['stats:unique_docs'] = 0
 
-    def add(self, token: str, document_id: str):
+    def add(self, token: str, document_id: str, tag=None):
         if token not in self.index:
             self.index[token] = {}
 
@@ -27,7 +27,10 @@ class Index:
         if document_id not in doc_list:
             doc_list[document_id] = Posting(document_id, 1)
         else:
-            doc_list[document_id] = Posting(document_id, doc_list[document_id].freq + 1)
+            doc_list[document_id].freq += 1
+
+        if tag is not None:
+            doc_list[document_id].importance_counts[tag] += 1
     
         self.index[token] = doc_list
         self.index.sync()
